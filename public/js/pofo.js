789f142ba18e907b916048e9ba0366cdf7917156
Rferng <rf8484@gmail.com>
2018-06-12 17:46:56 -0700

    fixed scroll up and added adwave

--- public/js/pofo.js
+++ public/js/pofo.js
@@ -1,18 +1,49 @@
+function smoothScroll(target,duration){
+	var target = document.querySelector(target);
+	var targetPosition = target.getBoundingClientRect().top;
+	var startPosition = window.pageYOffset;
+	var distance = targetPosition - startPosition;
+	var startTime = null;
+
+	function animation(currentTime){
+		if (startTime === null) startTime = currentTime;
+		var timeElapsed = currentTime - startTime;
+		var run = ease(timeElapsed,startPosition,distance,duration);
+		window.scrollTo(0,run);
+		if(timeElapsed < duration) requestAnimationFrame(animation);
+	}
+
+	function ease(t, b, c, d) {
+		t /= d/2;
+		if (t < 1) return c / 2 * t * t + b;
+		t--;
+		return -c / 2 * (t * (t - 2) - 1) + b;
+	}
+
+	requestAnimationFrame(animation);
+}
+
+var upBtn = document.querySelector('.upBtn');
+upBtn.addEventListener('click',function(){
+	smoothScroll('.top',1000);
+});
 
 $(window).scroll(function(){
 	if( $(window).scrollTop()>550){
-		$("a.scroll-to-top").fadeIn('fast');
+		$("a.upBtn").fadeIn('fast');
 	}else{
-		$("a.scroll-to-top").fadeOut('slow');
+		$("a.upBtn").fadeOut('slow');
 	}
 });
 
-$('a.scroll-to-top').click(function(){
-	$('body').animate({
-		scrollTop: 0
-	}, 500);
-	return false;
-});
+// $('a.scroll-to-top').click(function(){
+// 	$('body').animate({
+// 		scrollTop: 0
+// 	}, 500);
+// 	return false;
+// });
+
+//(target to animate to,duration)
 
 // window.onscroll = function() {scrollFunction()};
 
